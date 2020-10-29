afeaf83a056526df115d015652fad8602b3e46ff
HungNhOnPV <anhhungx9propro@gmail.com>
2020-10-29 18:24:48 +0700

    interceptors width response

--- src/main.js
+++ src/main.js
@@ -8,6 +8,11 @@ import vueResource from 'vue-resource'
 Vue.config.productionTip = false
 Vue.use(vueResource)
 Vue.http.options.root = 'https://vue-resource-cf573.firebaseio.com/users.json'
+Vue.http.interceptors.push((request, next) => {
+  console.log(request)
+  if (request.method === 'POST') request.method = 'PUT'
+  next()
+})
 
 /* eslint-disable no-new */
 new Vue({
