2bc5c994d9fdb8a16c638fe769135bdc3516148a
HungNhOnPV <anhhungx9propro@gmail.com>
2020-11-01 12:49:23 +0700

    splice vuex

--- src/store/store.js
+++ src/store/store.js
@@ -1,46 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import Result from './modules/Result'
+
+import * as getters from './Getters'
+import * as mutations from './Mutations'
+import * as actions from './Actions'
+
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
   state: {
-    result: 0,
     value: ''
   },
-  getters: {
-    tenResult: state => {
-      return state.result * 10
-    },
-    nameResult: state => {
-      return state.result + ' name product'
-    },
-    value: state => {
-      return state.value
-    }
-  },
-  mutations: {
-    increment: function (state, n) {
-      state.result++
-    },
-    decrement: state => {
-      state.result--
-    },
-    updateValue: (state, payload) => {
-      state.value = payload
-    }
-  },
-  actions: {
-    increment: ({ commit }) => {
-      commit('increment')
-    },
-    asyncDecrement: ({ commit }) => {
-      setTimeout(() => {
-        commit('decrement')
-      }, 2000)
-    },
-    updateValue: ({ commit }, payload) => {
-      commit('updateValue', payload)
-    }
+  getters,
+  mutations,
+  actions,
+  modules: {
+    Result
   }
 })
